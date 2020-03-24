a93eabefbd6276aa1c5cf7cad802c4913d00bfe0
atiyehghm <atiyeh1997>
2020-03-24 18:48:33 +0430

    changing the design of a textview

--- app/src/main/java/com/atiyeh/textdesigner/MainActivity.java
+++ app/src/main/java/com/atiyeh/textdesigner/MainActivity.java
@@ -3,6 +3,10 @@ package com.atiyeh.textdesigner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onRadioButtonClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+        TextView shadow = findViewById(R.id.shadowsample);
+        shadow.setVisibility(View.INVISIBLE);
+        TextView blocky = findViewById(R.id.blockysample);
+        blocky.setVisibility(View.INVISIBLE);
+        TextView glow = findViewById(R.id.glowsample);
+        glow.setVisibility(View.INVISIBLE);
+        switch(view.getId()){
+            case R.id.shadowradio:
+                if(checked) {
+                shadow.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.blockyradio:
+                if(checked) {
+                    blocky.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.glowradio:
+                if(checked) {
+                    glow.setVisibility(View.VISIBLE);
+                }
+                break;
+
+
+        }
     }
 }
